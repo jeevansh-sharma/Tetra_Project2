b009b7e6df4c9f28d136c6766265de2cbbe7acd1
jeevansh-sharma <jeevanshsharma15@gmail.com>
2024-12-05 19:57:35 +0530

    Added a hero parralax effect

--- app/page.tsx
+++ app/page.tsx
@@ -6,14 +6,15 @@ import Link from "next/link";
 import NavTabs from "@/components/animata/container/nav-tabs";
 import { SpinningTextCustomVariants } from "@/components/motionui/spining-text-custom";
 import { SpinningText } from "@/components/motionui/spinning-text";
+import { HeroParallaxDemo } from "./component/Home/HeroParallaxDemo";
 
 export default function Home() {
   return (
     
     <section className="max-w-7xl mx-auto flex flex-col">
-
+    <HeroParallaxDemo/>
   
-    <Link href="/director" className="mr-1 text-red-400">director</Link>
+    
    
    
     
